6cff398ea170ce2233e72b00d24894325c6745a6
gbseok <gbseok@gmail.com>
2024-08-29 17:26:29 +0900

    Update Jenkins file for test

--- container/container_build.sh
+++ container/container_build.sh
@@ -5,4 +5,4 @@ set -e
 cd $(dirname $0)
 
 # docker build -t sdi-pipeline-apps -f Dockerfile .
-docker buildx build --builder mybuilder --platform linux/amd64,linux/arm64 -t sdi-pipeline-apps -f Dockerfile .
+docker buildx build --builder builderx --platform linux/amd64,linux/arm64 -t sdi-pipeline-apps -f Dockerfile .
