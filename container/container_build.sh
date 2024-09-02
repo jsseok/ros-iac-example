26ab3bd8e436962476cbc86535f3962fc898915f
gbseok <gbseok@gmail.com>
2024-09-02 13:09:50 +0900

    Update container build shell for repo. push

--- container/container_build.sh
+++ container/container_build.sh
@@ -4,5 +4,4 @@ set -e
 
 cd $(dirname $0)
 
-# docker build -t sdi-pipeline-apps -f Dockerfile .
-docker buildx build --builder builderx --platform linux/amd64,linux/arm64 -t sdi-pipeline-apps -f Dockerfile .
+docker buildx build --builder builderx --platform=linux/amd64,linux/arm64 -t docker.io/jsseok/sdi-pipeline-apps:latest -f Dockerfile --push .
