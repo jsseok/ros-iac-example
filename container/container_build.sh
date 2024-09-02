64838c373043caa84647aa69a80b135cc7049d45
gbseok <gbseok@gmail.com>
2024-09-02 13:17:55 +0900

    Update container build shell for using cache

--- container/container_build.sh
+++ container/container_build.sh
@@ -4,4 +4,11 @@ set -e
 
 cd $(dirname $0)
 
-docker buildx build --builder builderx --platform=linux/amd64,linux/arm64 -t docker.io/jsseok/sdi-pipeline-apps:latest -f Dockerfile --push .
+docker buildx build \
+    --builder builderx \
+    --platform=linux/amd64,linux/arm64 \
+    -t docker.io/jsseok/sdi-pipeline-apps:latest \
+    -f Dockerfile \
+    --cache-from=type=registry,ref=docker.io/jsseok/sdi-pipeline-apps:cache \
+    --cache-to=type=registry,ref=docker.io/jsseok/sdi-pipeline-apps:cache,mode=max \
+    --push .
