acda12eff0a1c6036cc44af47fc67d8f314395d3
gbseok <gbseok@gmail.com>
2024-09-02 20:01:15 +0900

    Update build script for test

--- container/container_build.sh
+++ container/container_build.sh
@@ -6,7 +6,7 @@ cd $(dirname $0)
 
 docker buildx build \
     --builder builderx \
-    --platform=linux/amd64,linux/arm64 \
+    --platform linux/amd64,linux/arm64 \
     -t docker.io/jsseok/sdi-pipeline-apps:latest \
     -f Dockerfile \
     --cache-from=type=registry,ref=docker.io/jsseok/sdi-pipeline-apps:cache \
