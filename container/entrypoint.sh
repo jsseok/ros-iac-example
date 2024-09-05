a2c2796ba986d87d60ec0a00c5d941400d0a2ce1
gbseok <gbseok@gmail.com>
2024-09-05 09:33:34 +0900

    Update Dockerfile

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+set -e
+
+if [ "$(uname -m)" = "aarch64" ]; then
+    echo "ARM64 architecture"
+    if [ -d "/usr/local/cuda" ]; then       
+        echo "CUDA GPU"
+        pip install --no-cache-dir --ignore-installed torch torchvision onnx onnxruntime onnxruntime-gpu onnxslim opencv-python ultralytics
+    else
+        echo "No GPU"
+        pip install --no-cache-dir --ignore-installed torch torchvision onnx onnxruntime onnxslim opencv-python ultralytics --extra-index-url https://download.pytorch.org/whl/cpu
+    fi
+else
+    echo "AMD64 architecture"
+    pip install --no-cache-dir --ignore-installed torch torchvision onnx onnxruntime onnxruntime-gpu onnxslim opencv-python ultralytics
+fi
+
+source /opt/ros/${ROS_DISTRO}/setup.sh
+source /ros2_ws/sdi_pipeline/install/local_setup.sh
+
 yolo export model=yolov8s-seg.pt imgsz=640 format=onnx opset=12 simplify
 mkdir -p ~/sdi_models && mv ./yolov8s-seg.onnx ~/sdi_models && rm ./yolov8s-seg.pt
+
 exec "$@"
